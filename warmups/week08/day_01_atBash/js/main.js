31182251414c1edd7f0c112373f4bb52d889f619
Michello Lo <michelle.erica.lo@gmail.com>
2018-02-05 10:30:19 +1100

    W8D1

--- warmups/week08/day_01_atBash/js/main.js
+++ warmups/week08/day_01_atBash/js/main.js
@@ -1,1 +1,37 @@
 console.log("WARM?");
+
+const atbash = {
+
+  alphabet: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+
+
+  encode: function(input){
+    let output = '';
+
+    const str = input.toLowerCase().split('');
+    // =>["m", "i", "c", "h", "e", "l", "l", "e"]
+    const cipher = this.alphabet.slice().reverse()
+    // we need .slice() so this.alphabet does not destroy itself
+
+    for (let i = 0; i < str.length; i++) {
+      const index = this.alphabet.indexOf(str[i])
+      // debugger;
+      // console.log(index);
+
+      console.log(`REVERSE ALPHABET:${cipher}`);
+
+      let coded_letter = cipher[index];
+      // debugger;
+      output += coded_letter;
+
+    }
+    return output
+
+  }
+
+}
+
+atbash.encode('michelle')
+
+// input: "test"
+// output: 'gvhg'
